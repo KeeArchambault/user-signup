4e54aaafd7b8b629e9c8268235c7e16745f44523
Kee <keeandraa@gmail.com>
2019-06-20 17:04:09 -0500

    added usename and password error messages

--- main.py
+++ main.py
@@ -15,49 +15,50 @@ template_dir = os.path.join(os.path.dirname(__file__),
 jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
- index_form="""<form action="/">
-            <label for="username">Username:</label>
-            <input type="text" name="username" >{0}</input>
-        </form>
-
-         <form action="/password">
-            <label for="password">Password:</label>
-            <input type="text" name="password">{1}</input>
-        </form>
-
-        <form action="/verify">
-            <label for="verify">Verify Password:</label>
-            <input type="text" name="verify"></input>
-        </form>
-
-        <form action="/email">
-            <label for="email">Email:</label>
-            <input type="text" name="email"></input>
-        </form>
-    </body>
-
-"""
 
 @app.route("/")
 def index():
-    template= jinja_env.get_template("index.html")
-    return template.render().format()
+    if request.args.get("username_error"):
+        username_error= request.args.get("username_error")
+    else:
+        username_error= ""
 
-@app.route("/username", methods= ["post"])
+    if request.args.get("password_error"):
+        username_error= request.args.get("password_error")
+    else:
+        password_error= ""
+
+    template= jinja_env.get_template("index.html")
+    return template.render()
+
+@app.route("/username", methods= ["POST"])
 def username():
     username = request.form["username"]
-    if not "username":
-        username_error = "Please provide a username."
-        template= jinja_env.get_template("index.html")
-        return redirect("/?username_error={0}".format(username_error))
+    username_error = "Please provide a username."
+    template= jinja_env.get_template("/index.html")
 
-@app.route("/password", methods=["post"])
+    if not username:
+        return template.render(username_error=username_error)
+
+@app.route("/password", methods=["POST"])
 def password():
     password = request.form["password"]
-    if not "password":
-        password_error = "Please provide a password."
-        template= jinja_env.get_template("index.html")
-        return redirect("/?password_error={0}".format(password_error))        
+    password_error = "Please provide a password."
+    template= jinja_env.get_template("/index.html")
+
+    if not password:
+        return template.render(password_error=password_error)
+
+@app.route("/verify", methods=["POST"])
+def verify():
+    verify= request.form["verify"]
+    password = request.form["password"]
+    verify_error="Passwords do not match."
+
+    if password and verify != password:
+        return template.render(verify_error=verify_error)
+        
+
 
 app.run()    
     
